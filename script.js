bcab5cee04195b79a494cb15eda31cf8c42967ec
Mike White <myquite@gmail.com>
2020-08-11 16:21:08 -0500

    added a button to change quotes from an array

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("All good here!");
+const button = document.querySelector("#reload");
+const fact = document.querySelector("#fact");
+
+const orcaFacts = [
+  "Though called killer whales, orcas are not whales. They are the largest dolphin species.",
+  "In Argentina, orcas hurl themselves onshore to grab sea lion pups",
+  "In the Antartic, pod members cooperate to make large waves that wash seals from ice floes.",
+  "Whalers call the orca the 'killer of whales.' It preys on sperm, gray, fin, humpback, and other whales.",
+];
+
+button.addEventListener("click", (e) => {
+  e.preventDefault;
+  let randomValue = orcaFacts[Math.floor(Math.random() * orcaFacts.length)];
+  fact.classList.add("fade");
+  fact.innerText = randomValue;
+  setTimeout(() => {
+    fact.classList.remove("fade");
+  }, 500);
+});
